7e69b261c1e11bc1d059383357f6ba8e35938e40
Surya Pratap <85509906+singham2000@users.noreply.github.com>
2024-01-29 17:34:21 +0530

    Added routes in admin route, and adminController register admin

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -42,6 +42,59 @@ exports.registerDoctor = catchAsyncError(async (req, res, next) => {
     sendData(user, 200, res);
 });
 
+exports.registerAdmin = catchAsyncError(async (req, res, next) => {
+    const {
+        fullname, email, password, validator
+    } = req.body;
+    if (validator != "ares") {
+        return next(new ErrorHandler("Unauthorized!", 400));
+    }
+    if (!fullname || !email || !password) {
+        return next(new ErrorHandler("Please fill all fields", 400));
+    }
+
+    let user = await userModel.findOne({ email });
+
+    if (user)
+        return next(new ErrorHandler("User already exists with this email", 400));
+    if (password.length < 8)
+        return next(
+            new ErrorHandler("Password should have minimum 8 characters", 400)
+        );
+
+    user = await userModel.create({
+        fullname, email, password, role: "admin"
+    });
+
+    await user.save();
+
+    user.password = undefined;
+    sendData(user, 200, res);
+});
+
+exports.login = catchAsyncError(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password)
+        return next(new ErrorHandler("Please enter your email and password", 400));
+
+    const user = await userModel.findOne({ email }).select("+password");
+
+    if (user.role !== "admin")
+        return next(new ErrorHandler("Unauthorized user login.", 401));
+
+    if (!user) {
+        return next(new ErrorHandler("Invalid email or password", 401));
+    }
+
+    const isPasswordMatched = await user.comparePassword(password);
+    if (!isPasswordMatched)
+        return next(new ErrorHandler("Invalid email or password!", 401));
+
+    user.password = undefined;
+    sendData(user, 200, res);
+});
+
 exports.evaluationFormMake = catchAsyncError(async (req, res) => {
     const { fieldName, values } = req.body;
     try {
@@ -62,4 +115,24 @@ exports.evaluationFormMake = catchAsyncError(async (req, res) => {
     } catch (err) {
         return next(new ErrorHandler(err, 400));
     }
+});
+
+exports.getAllDoc = catchAsyncError(async (req, res) => {
+
+    const page = parseInt(req.query.page_no) || 1;
+    const limit = parseInt(req.query.per_page_count) || 10;
+    const query = {};
+    query.role = 'doctor';
+    const doctors = await userModel.find(query)
+        .sort({ createdAt: 'desc' })
+        .skip((page - 1) * limit)
+        .limit(limit)
+        .exec();
+
+    const totalRecords = await userModel.countDocuments(query);
+    res.json({
+        data: doctors,
+        totalPages: Math.ceil(totalRecords / limit),
+        currentPage: page,
+    });
 });
--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const { auth } = require('../middlewares/auth');
-const { registerDoctor } = require('../controllers/adminController');
+const {
+    registerDoctor,
+    registerAdmin,
+    login,
+    getAllDoc
+} = require('../controllers/adminController');
 
 const router = express.Router();
 router.post("/register_doctor", registerDoctor);
+router.post("/register_admin", registerAdmin);
+router.post("/login", login);
+router.get("/get_all_doctor", getAllDoc)
 
 module.exports = router;
