01629b05307862adbacf5853205ed391a611bcbc
Surya Pratap <85509906+singham2000@users.noreply.github.com>
2024-02-02 11:57:20 +0530

    schema fix

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -26,7 +26,7 @@ const userSchema = new mongoose.Schema({
   role: {
     type: String,
     enum: roleEnum,
-    default: "Athelete",
+    default: "athelete",
   },
   temp_code: {
     type: String,
