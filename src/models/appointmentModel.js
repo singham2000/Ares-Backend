06df1fde5aed9b7c19342374428160fb84be3327
Surya Pratap <85509906+singham2000@users.noreply.github.com>
2024-03-14 11:15:22 +0530

    updates

--- src/models/appointmentModel.js
+++ src/models/appointmentModel.js
@@ -8,7 +8,7 @@ const appointmentServiceEnum = [
   "ConcussionEval",
 ];
 const paymentStatus = ["paid", "pending", "failed"];
-const appointmentStatus = ["completed", "upcoming", "cancel"];
+const appointmentStatus = ["completed", "upcoming", "cancelled"];
 
 const appointmentSchema = new mongoose.Schema(
   {
