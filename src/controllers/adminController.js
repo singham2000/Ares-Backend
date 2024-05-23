f35d316ca486bfb91d636ba3f2d71c95de82258c
Surya Pratap <85509906+singham2000@users.noreply.github.com>
2024-05-23 13:35:09 +0530

    updates

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -94,10 +94,6 @@ exports.registerDoctor = catchAsyncError(async (req, res, next) => {
 
   if (user)
     return next(new ErrorHandler("User already exists with this email", 400));
-  if (password.length < 8)
-    return next(
-      new ErrorHandler("Password should have minimum 8 characters", 400),
-    );
 
   user = await userModel.create({
     firstName,
